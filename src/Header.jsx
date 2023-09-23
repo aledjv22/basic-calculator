4d43b77e6fb4d81533c50911e721978555b047c9
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2023-09-23 00:39:45 -0300

    created header

---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+function Header(){
+    return(
+        <header >
+            <h1 className='text-[#FF0000] text-[70px] m-0 font-[oblique] font-bold'>Basic Calculator</h1>
+        </header>
+    )
+}
+
+export default Header;
