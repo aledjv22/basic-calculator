5257b5780cf9bf60d5f4405511c3cf8dde58e903
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2023-09-23 00:45:55 -0300

    operation buttons

---
+++ src/Operations.jsx
@@ -0,0 +1,24 @@
+function Operations() {
+  return (
+    <div>
+      <div className='mb-2 mt-2'>
+        <button className=' bg-violet-500 p-1 rounded-lg w-[90px] mr-5'>
+          Addition
+        </button>
+        <button className=' bg-violet-500 p-1 rounded-lg w-[90px] ml-5'>
+          Subtraction
+        </button>
+      </div>
+      <div>
+        <button className=' bg-violet-500 p-1 rounded-lg w-[90px] mr-5'>
+          Multiply
+        </button>
+        <button className=' bg-violet-500 p-1 rounded-lg w-[90px] ml-5'>
+          Divide
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Operations;
