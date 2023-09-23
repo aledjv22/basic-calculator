1dfe185f363d63e31cbda2d49429d7d74a0a167e
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2023-09-23 00:38:54 -0300

    App Structure

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./Header";
+import Center from "./Center";
+
+function App() {
+  return (
+    <div className='text-white text-center'>
+      <Header />
+      <Center />
+    </div>
+  );
+}
+
+export default App
